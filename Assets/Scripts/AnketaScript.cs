9d00a978cdfb7b8c95bf9fb03dac8c6fd6eada25
Vanysha22829 <gribkovivan121mail.ru>
2024-11-09 19:35:00 +0300

    popa

--- Assets/Scripts/AnketaScript.cs
+++ Assets/Scripts/AnketaScript.cs
@@ -7,10 +7,17 @@ public class AnketaScript : MonoBehaviour
 {
     public TMP_InputField nameInputField;
     public TMP_Text nameText; 
-   
+    public TMP_InputField ageInputField
+    public TMP_Text ageText
+
     // Update is called once per frame
     public void OnButtonUserName()
     {
         nameText.text = nameInputField.text;
     }
-}
+
+    public void OnButtonUserAge()
+    {
+        string ageString = ageInputField.text;
+
+    }
