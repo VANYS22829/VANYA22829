85b53748f0c3676cff399420563dc505321715e6
Vanysha22829 <gribkovivan121mail.ru>
2024-11-27 19:48:35 +0300

    uwu

--- Assets/Scripts/CalculatorScript.cs
+++ Assets/Scripts/CalculatorScript.cs
@@ -1,70 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMP;
+using TMPro;
 using System.Runtime.CompilerServices;
+using TMPro;
+using System;
 public class CalculatorScript : MonoBehaviour
 {
-    {SerializeField} private TMP_InputField inputField1;
-    {SerializeField} private TMP_InputField 2; 
-    {SerializeField} private TMP_Text outPutText;
-    {SerializeField} private int value1;
-    {SerializeField} private int value2;
+    [SerializeField] private TMP_InputField inputField1;
+    [SerializeField] private TMP_InputField inputField2; 
+    [SerializeField] private TMP_Text outPutText;
+    [SerializeField] private float value1;
+    [SerializeField] private float value2;
     public void onButtonMinus()
     {
-       int value1 = ReadIntFromInputField(inputField1);
-       int value2 = ReadIntFromInputField(inputField2);
+       float value1 = ReadFloatFromInputField(inputField1);
+       float value2 = ReadFloatFromInputField(inputField2);
        outPutText.text = (value1 - value2).ToString();
     }
     public void onButtonPlus()
     {
-       int value1 = ReadIntFromInputField(inputField1);
-       int value2 = ReadIntFromInputField(inputField2);
+       float value1 = ReadFloatFromInputField(inputField1);
+       float value2 = ReadFloatFromInputField(inputField2);
        outPutText.text = (value1 + value2).ToString();
     }
     public void onButtonMultiply()
     {
-       int value1 = ReadIntFromInputField(inputField1);
-       int value2 = ReadIntFromInputField(inputField2);
+       float value1 = ReadFloatFromInputField(inputField1);
+       float value2 = ReadFloatFromInputField(inputField2);
        outPutText.text = (value1 * value2).ToString();
     }
     public void onButtonDivide()
     {
-     float value1 = ReadIntFromInputField(inputField1);
-     float value2 = ReadIntFromInputField(inputField2);
+     float value1 = ReadFloatFromInputField(inputField1);
+     float value2 = ReadFloatFromInputField(inputField2);
      if (value2 == 0) {
         outPutText.text = "0 division is impossimble";
      } else {
         outPutText.text = (value1 / value2).ToString();
      }
     }
-    private int ReadIntFromInputField (TMP_InputField InputField) {
-       string str inputField.text;
-       if (str != null && str.Lenght > 0) {
-            if (int.TryParse(str, out int intValue)) {
-                return intValue
+
+float ReadFloatFromInputField(object inputField2)
+{
+    throw new NotImplementedException();
+}
+
+private float ReadFloatFromInputField (TMP_InputField inputField) {
+       string str = inputField.text;
+       if (str != null && str.Length > 0) {
+            if (float.TryParse(str, out float floatValue)) {
+                return floatValue;
     }                                    
 }
 return 0;
 }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
